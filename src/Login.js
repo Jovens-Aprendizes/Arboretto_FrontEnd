f38e8d5b9e9db296a2a457370a0141a1b7635608
CardosoPLeo <leonardo_p_c@hotmail.com>
2023-09-11 18:54:43 -0300

    Alterações e inclusão de nova logo

--- src/Login.js
+++ src/Login.js
@@ -14,14 +14,15 @@ const Login = () => {
         <div className="col-md-6 p-0" style={{ backgroundColor: '#D8F3DC', height: '100vh' }}>
           <div className="d-flex flex-column justify-content-center align-items-center h-100">
             <h1 style={{color: 'black', fontSize: 50, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>A R B O R E T T O</h1>
-            <img src={logo} alt="Logo(Árvore)" width="450" height="300"
+            <img src={logo} alt="Logo(Árvore)" width="600" height="800"
             style={{
-              right:'70px',
+              right:'0',
+              top:'10px',
               position:'relative',
               display:'flex'
             }}
             />
-            <div style={{width: 648.01, height: 0, border: '0.50px rgba(170, 170, 170, 0.80) solid'}}></div>
+            {/* <div style={{width: 648.01, height: 0, border: '0.50px rgba(170, 170, 170, 0.80) solid'}}></div> */}
           </div>
         </div>
         <div className="col-md-6">
@@ -41,7 +42,7 @@ const Login = () => {
                   textAlign:'center'
 
                 }}
-                placeholder="E-mail"
+                placeholder="Informe seu CPF"
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
               />
