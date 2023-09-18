3685f51fea2b4164a4c26fd14eca75bbbabb99b0
David Siqueira <davidsiqueira.hog@gmail.com>
2023-09-17 23:34:57 -0300

    initial commit (migração next)

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,41 +1,42 @@
-// auth.js (ou AuthService.js)
-class AuthService {
-    async login(cpf, password) {
-      try {
-        // Chame a API para autenticar o usuário (substitua 'apiEndpoint' pelo seu URL real)
-        const response = await fetch('apiEndpoint/auth/login', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ cpf, password }),
-        });
-  
-        if (response.ok) {
-          const data = await response.json();
-          // Armazene o token de autenticação em localStorage ou cookies
-          localStorage.setItem('token', data.token);
-          return true; // Login bem-sucedido
-        } else {
-          return false; // Falha no login
-        }
-      } catch (error) {
-        console.error('Erro ao autenticar:', error);
-        return false; // Falha no login devido a um erro
+import axios from "axios";
+
+export async function login(cpf, password) {
+  try {
+    const response = await axios.get(
+      "https://arborettoapi.azurewebsites.net/api-arboretto-dev/v1/usuario/login",
+      {
+        headers: {
+          cpf: cpf,
+          senha: password,
+        },
       }
+    );
+
+    if (response) {
+      const data = response.data;
+      if (typeof window !== "undefined") {
+        localStorage.setItem("user", JSON.stringify(data));
+      }
+      return true;
+    } else {
+      return false;
     }
-  
-    logout() {
-      // Remova o token de autenticação (se necessário)
-      localStorage.removeItem('token');
-    }
-  
-    isAuthenticated() {
-      // Verifique se o usuário está autenticado com base no token (ou outras verificações)
-      const token = localStorage.getItem('token');
-      return !!token; // Retorna true se o token existir
-    }
+  } catch (error) {
+    console.error("Erro ao autenticar:", error);
+    return false;
   }
-  
-  export default new AuthService();
-  
+}
+
+export function logout() {
+  if (typeof window !== "undefined") {
+    localStorage.removeItem("user");
+  }
+}
+
+export function isAuthenticated() {
+  if (typeof window !== "undefined") {
+    const token = localStorage.getItem("user");
+    return !!token;
+  }
+  return false;
+}
