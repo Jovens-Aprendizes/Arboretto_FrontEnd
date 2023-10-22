d8e5ffb9b2800043ec4d9e928478450b6efebdef
Leonardo Cardoso <leonardo.devops@alterdata.com.br>
2023-10-22 20:18:06 -0300

    Finalizado a página home.

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,15 +1,36 @@
 import { GetServerSideProps } from "next";
 import { parseCookies } from "nookies";
-import Dados from "../components/dados/dados";
+// import Dados from "../components/dados/dados";
 import Navbar from "../components/sidebar/sidebar";
+import { Heading, Stack } from "@chakra-ui/react";
 
 
 const Home = () => {
 
-
   return (
     <Navbar>
-      <Dados/>
+          <Stack spacing="6" textAlign="center">
+            <Heading
+              fontSize="50"
+              fontFamily="Inter"
+              fontWeight="400"
+            >
+              A R B O R E T T O
+            </Heading>
+            <img
+              src="/logo.png"
+              alt="Logo(Árvore)"
+              width="600"
+              height="800"
+              style={{
+                right: "0",
+                top: "10px",
+                left: "370px",
+                position: "relative",
+                display: "block",
+              }}
+            />
+          </Stack>
     </Navbar>
   );
 };
