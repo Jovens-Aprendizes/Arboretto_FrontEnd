8f97ae572e7dc615a6ee0a911cfee2ec97196f17
David Siqueira <davidsiqueira.hog@gmail.com>
2023-11-06 18:12:51 -0300

    chart fix

--- src/components/bigdata/chart_bigdata.tsx
+++ src/components/bigdata/chart_bigdata.tsx
@@ -1,30 +1,32 @@
-import { ApexOptions } from 'apexcharts';
-import axios from 'axios';
-import React, { useState, useEffect } from 'react';
-import ReactApexChart from 'react-apexcharts';
+import dynamic from 'next/dynamic'
+const ReactApexChart = dynamic(() => import('react-apexcharts'), { ssr: false });
+import { ApexOptions } from "apexcharts";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+
 
 const Chart = () => {
   const currentMonth = new Date().getMonth() + 1;
   const currentYear = new Date().getFullYear();
 
   const monthOptions = [
-    { value: '1', label: 'Janeiro' },
-    { value: '2', label: 'Fevereiro' },
-    { value: '3', label: 'Março' },
-    { value: '4', label: 'Abril' },
-    { value: '5', label: 'Maio' },
-    { value: '6', label: 'Junho' },
-    { value: '7', label: 'Julho' },
-    { value: '8', label: 'Agosto' },
-    { value: '9', label: 'Setembro' },
-    { value: '10', label: 'Outubro' },
-    { value: '11', label: 'Novembro' },
-    { value: '12', label: 'Dezembro' },
+    { value: "1", label: "Janeiro" },
+    { value: "2", label: "Fevereiro" },
+    { value: "3", label: "Março" },
+    { value: "4", label: "Abril" },
+    { value: "5", label: "Maio" },
+    { value: "6", label: "Junho" },
+    { value: "7", label: "Julho" },
+    { value: "8", label: "Agosto" },
+    { value: "9", label: "Setembro" },
+    { value: "10", label: "Outubro" },
+    { value: "11", label: "Novembro" },
+    { value: "12", label: "Dezembro" },
   ];
 
   const yarsOptions = [
-    { value: '2023', label: '2023' },
-    { value: '2024', label: '2024' },
+    { value: "2023", label: "2023" },
+    { value: "2024", label: "2024" },
   ];
 
   const [selectedMonth, setSelectedMonth] = useState(currentMonth);
@@ -32,7 +34,7 @@ const Chart = () => {
   const [chartData, setChartData] = useState([]);
   const [options, setOptions] = useState({
     chart: {
-      type: 'bar',
+      type: "bar",
     },
     xaxis: {
       categories: [],
@@ -55,16 +57,16 @@ const Chart = () => {
   const updateChartData = async () => {
     try {
       const dados = await GetDiasAgendados(selectedMonth, selectedYear);
-      
+
       const daysOfWeek = {};
-      
+
       dados.forEach((item) => {
         const { nomeDiaDaSemana, totalReservas } = item;
-        
+
         if (!daysOfWeek[nomeDiaDaSemana]) {
           daysOfWeek[nomeDiaDaSemana] = 0;
         }
-        
+
         daysOfWeek[nomeDiaDaSemana] += totalReservas;
       });
 
@@ -110,25 +112,31 @@ const Chart = () => {
             {monthOptions.map((option) => (
               <option key={option.value} value={option.value}>
                 {option.label}
-                </option>
+              </option>
             ))}
           </select>
-          
         </div>
         <div>
           <label htmlFor="year">Ano:</label>
           <select id="year" value={selectedYear} onChange={handleYearChange}>
-          {yarsOptions.map((option) => (
+            {yarsOptions.map((option) => (
               <option key={option.value} value={option.value}>
                 {option.label}
-                </option>
+              </option>
             ))}
           </select>
         </div>
         {/* <button type="submit">Filtrar</button> */}
       </form>
       <div id="chart">
-        <ReactApexChart options={options} series={[{ data: chartData }]} type="bar" height={450} />
+        {typeof window !== "undefined" && (
+          <ReactApexChart
+            options={options}
+            series={[{ data: chartData }]}
+            type="bar"
+            height={450}
+          />
+        )}
       </div>
     </div>
   );
