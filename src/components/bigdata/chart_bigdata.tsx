bd906680c966166fe52a174adcd53b0a44672ef6
Leonardo Cardoso <leonardo.devops@alterdata.com.br>
2023-11-05 23:12:20 -0300

    Criação dos gráficos do big data

---
+++ src/components/bigdata/chart_bigdata.tsx
@@ -0,0 +1,136 @@
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import ReactApexChart from 'react-apexcharts';
+
+const Chart = () => {
+  const currentMonth = new Date().getMonth() + 1;
+  const currentYear = new Date().getFullYear();
+
+  const monthOptions = [
+    { value: '1', label: 'Janeiro' },
+    { value: '2', label: 'Fevereiro' },
+    { value: '3', label: 'Março' },
+    { value: '4', label: 'Abril' },
+    { value: '5', label: 'Maio' },
+    { value: '6', label: 'Junho' },
+    { value: '7', label: 'Julho' },
+    { value: '8', label: 'Agosto' },
+    { value: '9', label: 'Setembro' },
+    { value: '10', label: 'Outubro' },
+    { value: '11', label: 'Novembro' },
+    { value: '12', label: 'Dezembro' },
+  ];
+
+  const yarsOptions = [
+    { value: '2023', label: '2023' },
+    { value: '2024', label: '2024' },
+  ];
+
+  const [selectedMonth, setSelectedMonth] = useState(currentMonth);
+  const [selectedYear, setSelectedYear] = useState(currentYear);
+  const [chartData, setChartData] = useState([]);
+  const [options, setOptions] = useState({
+    chart: {
+      type: 'bar',
+    },
+    xaxis: {
+      categories: [],
+    },
+  });
+
+  const handleMonthChange = (e) => {
+    setSelectedMonth(e.target.value);
+  };
+
+  const handleYearChange = (e) => {
+    setSelectedYear(e.target.value);
+  };
+
+  const handleFilterSubmit = (e) => {
+    e.preventDefault();
+    updateChartData();
+  };
+
+  const updateChartData = async () => {
+    try {
+      const dados = await GetDiasAgendados(selectedMonth, selectedYear);
+      
+      const daysOfWeek = {};
+      
+      dados.forEach((item) => {
+        const { nomeDiaDaSemana, totalReservas } = item;
+        
+        if (!daysOfWeek[nomeDiaDaSemana]) {
+          daysOfWeek[nomeDiaDaSemana] = 0;
+        }
+        
+        daysOfWeek[nomeDiaDaSemana] += totalReservas;
+      });
+
+      const categories = Object.keys(daysOfWeek);
+      const data = Object.values(daysOfWeek);
+
+      const newOptions = {
+        ...options,
+        xaxis: {
+          categories: categories,
+        },
+      };
+
+      setChartData(data);
+      setOptions(newOptions);
+    } catch (error) {
+      console.error("Erro ao buscar dados da API:", error);
+    }
+  };
+
+  async function GetDiasAgendados(mes, ano) {
+    try {
+      const response = await axios.get(
+        `https://api-arboretto-production.up.railway.app/api-arboretto-dev/v1/dia-demandado/listar-dias-demandados?mes=${mes}&ano=${ano}`
+      );
+      return response.data;
+    } catch (error) {
+      console.error("Erro ao buscar dados da API:", error);
+      throw error;
+    }
+  }
+
+  useEffect(() => {
+    updateChartData();
+  }, [selectedMonth, selectedYear]);
+
+  return (
+    <div>
+      <form onSubmit={handleFilterSubmit}>
+        <div>
+          <label htmlFor="month">Mês:</label>
+          <select id="month" value={selectedMonth} onChange={handleMonthChange}>
+            {monthOptions.map((option) => (
+              <option key={option.value} value={option.value}>
+                {option.label}
+                </option>
+            ))}
+          </select>
+          
+        </div>
+        <div>
+          <label htmlFor="year">Ano:</label>
+          <select id="year" value={selectedYear} onChange={handleYearChange}>
+          {yarsOptions.map((option) => (
+              <option key={option.value} value={option.value}>
+                {option.label}
+                </option>
+            ))}
+          </select>
+        </div>
+        {/* <button type="submit">Filtrar</button> */}
+      </form>
+      <div id="chart">
+        <ReactApexChart options={options} series={[{ data: chartData }]} type="bar" height={450} />
+      </div>
+    </div>
+  );
+};
+
+export default Chart;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,55 +1,32 @@
-import { GetServerSideProps } from "next";
-import { parseCookies } from "nookies";
-// import Dados from "../components/dados/dados";
+import React from "react";
 import Navbar from "../components/sidebar/sidebar";
+import ChartBigData from "../components/bigdata/chart_bigdata";
 import { Heading, Stack } from "@chakra-ui/react";
 
-
 const Home = () => {
-
   return (
     <Navbar>
-          <Stack spacing="6" textAlign="center">
-            <Heading
-              fontSize="50"
-              fontFamily="Inter"
-              fontWeight="400"
-            >
-              A R B O R E T T O
-            </Heading>
-            <img
-              src="/logo.png"
-              alt="Logo(Árvore)"
-              width="600"
-              height="800"
-              style={{
-                right: "0",
-                top: "10px",
-                left: "370px",
-                position: "relative",
-                display: "block",
-              }}
-            />
-          </Stack>
+      <ChartBigData />
+      {/* <Stack spacing="6" textAlign="center">
+        <Heading fontSize="50" fontFamily="Inter" fontWeight="400">
+          A R B O R E T T O
+        </Heading>
+        <img
+          src="/logo.png"
+          alt="Logo(Árvore)"
+          width="600"
+          height="800"
+          style={{
+            right: "0",
+            top: "10px",
+            left: "370px",
+            position: "relative",
+            display: "block",
+          }}
+        /> 
+      </Stack> */}
     </Navbar>
   );
 };
 
 export default Home;
-
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const { "arboretto-token": token } = parseCookies(ctx);
-
-  if (!token) {
-    return {
-      redirect: {
-        destination: "/",
-        permanent: false,
-      },
-    };
-  }
-
-  return {
-    props: {},
-  };
-};
